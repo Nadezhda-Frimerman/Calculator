b1b049aa25b4ba8744f9aeda1d4295ae5dedae42
Nadine273 <tomns@mail.ru>
2024-09-29 21:48:08 +0200

    Homework is done

---
+++ src/main/java/ru_Nadine_calculator/Controller/CalculatorController.java
@@ -0,0 +1,47 @@
+package ru_Nadine_calculator.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru_Nadine_calculator.Service.CalculatorService;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping ()
+    public String getHello (){
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping(path = "/plus")
+    public String getPlus (@RequestParam (value = "num1") Double num1,
+                           @RequestParam (value = "num2") Double num2){
+        return num1+" + "+num2+"="+calculatorService.getSummary (num1, num2);
+
+    }
+
+    @GetMapping(path = "/minus")
+    public String getMinus (@RequestParam (value = "num1") Double num1,
+                            @RequestParam (value = "num2") Double num2){
+        return num1+" - "+num2+" = "+ calculatorService.getSubtraction (num1, num2);
+
+    }
+    @GetMapping(path = "/multiply")
+    public String getMultiply (@RequestParam (value = "num1") Double num1,
+                               @RequestParam (value = "num2") Double num2){
+        return num1+" + "+num2+" = "+ calculatorService.getMultiplication (num1, num2);
+    }
+    @GetMapping(path = "/divide")
+    public String getDivide (@RequestParam (value = "num1") Double num1,
+                             @RequestParam (value = "num2") Double num2){
+        return num1+" / "+num2+" = "+ calculatorService.getDivision (num1, num2);
+    }
+
+}
---
+++ src/main/java/ru_Nadine_calculator/Service/CalculatorService.java
@@ -0,0 +1,36 @@
+package ru_Nadine_calculator.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.text.Format;
+
+@Service
+public class CalculatorService {
+    public Double getSummary (Double num1, Double num2){
+        if (num1==null||num2==null){
+            throw new IllegalArgumentException("Аргумент не передан");
+        }
+        return num1+num2;
+    }
+    public Double getSubtraction (Double num1, Double num2){
+        if (num1==null||num2==null){
+            throw new IllegalArgumentException("Аргумент не передан");
+        }
+        return num1-num2;
+    }
+    public Double getMultiplication (Double num1, Double num2){
+        if (num1==null||num2==null){
+            throw new IllegalArgumentException("Аргумент не передан");
+        }
+       return num1*num2;
+    }
+    public Double getDivision (Double num1, Double num2){
+        if (num1==null||num2==null){
+            throw new IllegalArgumentException("Аргумент не передан");
+        }
+        if (num2==0.0){
+            throw new IllegalArgumentException("Делить на ноль нельзя");
+        }
+        return num1/num2;
+    }
+}
